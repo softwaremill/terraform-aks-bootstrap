0572599d94c71419810d4c17c08a047e92257737
Wiktor Wojda <wiktor.wojda@gmail.com>
2022-09-19 11:52:03 +0200

    terraform fmt

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
 # This group have to be created manually
 data "azuread_group" "aks_cluster_admins" {
-  count = var.use_cluster_admins_group ? 1 : 0
+  count        = var.use_cluster_admins_group ? 1 : 0
   display_name = var.admins_group_name
 }
 
