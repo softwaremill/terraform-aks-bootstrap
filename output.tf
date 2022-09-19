a321044b92a0f0b8dca81a532cd7d50d835404cb
Wiktor Wojda <wiktor.wojda@gmail.com>
2022-09-19 12:47:38 +0200

    Added additional needed outputs

--- output.tf
+++ output.tf
@@ -2,6 +2,21 @@ output "cluster_name" {
   value = var.cluster_name
 }
 
+output "cluster_id" {
+  description = "The azurerm_kubernetes_cluster's id."
+  value       = module.aks.aks_id
+}
+
+output "host" {
+  description = "The host in the azurerm_kubernetes_cluster's kube_config block. The Kubernetes cluster server host."
+  value       = module.aks.host
+}
+
+output "kubernetes_ca_certificate" {
+  description = "The cluster_ca_certificate in the azurerm_kubernetes_cluster's kube_admin_config block. Base64 encoded public CA certificate used as the root of trust for the Kubernetes cluster."
+  value       = module.aks.admin_cluster_ca_certificate
+}
+
 output "resource_group_name" {
   value = var.resource_group_name
 }
