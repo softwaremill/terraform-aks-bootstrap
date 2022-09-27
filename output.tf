bd562cbea7112fadf5e7437fb6cc8cf8882ca7b7
Wiktor Wojda <wiktor.wojda@gmail.com>
2022-09-27 15:32:14 +0200

    Feat/sensitive true to outputs (#7)
    * Added sensitive outputs
    
    * Added client certificates to output
    
    * Added more outputs needed to auth to cluster
    
    * removed duplication after conflict resolving
    
    * terraform-docs: automated action
    
    * Fixed return value
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- output.tf
+++ output.tf
@@ -8,6 +8,42 @@ output "cluster_id" {
   value       = module.aks.aks_id
 }
 
+output "admin_client_certificate" {
+  description = "The `client_certificate` in the `azurerm_kubernetes_cluster`'s `kube_admin_config` block.  Base64 encoded public certificate used by clients to authenticate to the Kubernetes cluster."
+  sensitive   = true
+  value       = module.aks.admin_client_certificate
+}
+
+output "admin_client_key" {
+  description = "The `client_key` in the `azurerm_kubernetes_cluster`'s `kube_admin_config` block. Base64 encoded private key used by clients to authenticate to the Kubernetes cluster."
+  sensitive   = true
+  value       = module.aks.admin_client_key
+}
+
+output "admin_cluster_ca_certificate" {
+  description = "The `cluster_ca_certificate` in the `azurerm_kubernetes_cluster`'s `kube_admin_config` block. Base64 encoded public CA certificate used as the root of trust for the Kubernetes cluster."
+  sensitive   = true
+  value       = module.aks.admin_cluster_ca_certificate
+}
+
+output "admin_host" {
+  description = "The `host` in the `azurerm_kubernetes_cluster`'s `kube_admin_config` block. The Kubernetes cluster server host."
+  sensitive   = true
+  value       = module.aks.admin_host
+}
+
+output "admin_password" {
+  description = "The `password` in the `azurerm_kubernetes_cluster`'s `kube_admin_config` block. A password or token used to authenticate to the Kubernetes cluster."
+  sensitive   = true
+  value       = module.aks.admin_password
+}
+
+output "admin_username" {
+  description = "The `username` in the `azurerm_kubernetes_cluster`'s `kube_admin_config` block. A username used to authenticate to the Kubernetes cluster."
+  sensitive   = true
+  value       = module.aks.admin_username
+}
+
 output "host" {
   description = "The host in the azurerm_kubernetes_cluster's kube_config block. The Kubernetes cluster server host."
   sensitive   = true
@@ -30,6 +66,18 @@ output "cluster_ca_certificate" {
   description = "The `cluster_ca_certificate` in the `azurerm_kubernetes_cluster`'s `kube_config` block. Base64 encoded public CA certificate used as the root of trust for the Kubernetes cluster."
   sensitive   = true
   value       = module.aks.cluster_ca_certificate
+}
+
+output "username" {
+  description = "The `username` in the `azurerm_kubernetes_cluster`'s `kube_config` block. A username used to authenticate to the Kubernetes cluster."
+  sensitive   = true
+  value       = module.aks.username
+}
+
+output "password" {
+  description = "The `password` in the `azurerm_kubernetes_cluster`'s `kube_config` block. A password or token used to authenticate to the Kubernetes cluster."
+  sensitive   = true
+  value       = module.aks.client_certificate
 }
 
 output "resource_group_name" {
