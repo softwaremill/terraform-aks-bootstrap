6ed561c2a35a4df15e7cca19f08fe26792dafd2d
abielawa <aleksandra.bielawa@softwaremill.com>
2022-12-30 13:53:32 +0100

    Add use_for_each variable

--- network.tf
+++ network.tf
@@ -1,6 +1,8 @@
 module "network" {
   source              = "Azure/network/azurerm"
+  version             = "~> 5.0"
   resource_group_name = azurerm_resource_group.cluster.name
+  use_for_each        = var.use_for_each
   address_space       = var.address_space
   subnet_prefixes     = var.subnet_prefixes
   subnet_names        = var.subnet_names
